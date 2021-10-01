fd88fe8cf66af397db9606c8eeed9e74ec900363
sid0dji <eliass009@inbox.lv>
2021-10-01 10:46:02 +0300

    Update darbibas.py

---
+++ darbibas.py
@@ -0,0 +1,40 @@
+skaitlis1=4
+skaitlis2=3
+
+saskaitisana=skaitlis1+skaitlis2
+print(saskaitisana)
+
+atnemsana=skaitlis1-skaitlis2
+print(atnemsana)
+
+reizinasana=skaitlis1*skaitlis2
+print(reizinasana)
+
+dalisana=skaitlis1/skaitlis2
+print(dalisana)
+
+
+dalisana2=skaitlis1//skaitlis2
+print(dalisana2)#dalisana bez atlikuma
+
+atlikums=skaitlis1%skaitlis2
+print(atlikums)
+
+pakape=skaitlis1**skaitlis2
+print(pakape)#pakapes noteiksana ar zimi **
+
+#salidzinasana
+print(10>9)#taisniba
+print(2==4)#nav taisniba
+a=2
+a1="2"
+print(a==a1)#nav vienads jo a ir skaitlis, a1 teksts
+
+#!= nav vienads
+#>= lielaks vai vienads
+
+# and &&
+#japasaka, ka x mainigais atrodas starp 5 un 10, ka to pierakstit?
+#x>5 && x<10
+
+#or ||
