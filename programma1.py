e7ae99b0703bd52a370745db2d85a17cd3a2c226
sid0dji <eliass009@inbox.lv>
2021-10-01 10:03:32 +0300

    ievads ar vardiem un funkcijam

--- programma1.py
+++ programma1.py
@@ -1,6 +1,20 @@
 #print("hello world")
 print("Eliass")
+
 skaitlis=5
 vardi="Skola"
 print(skaitlis)
 print(vardi)
+
+v1 , v2 , v3 = "saule", "vejs", "udens"
+print(v1, v2, v3)
+
+m1=m2=m3="viens simbols"
+print(m1 , m2 , m3)#parversam 3 mainigos par 1  izvadi
+
+def nosaukumi(name):
+    print("sveiks", name)
+
+nosaukumi("vecais")
+nosaukumi("draugs")
+nosaukumi("elias")#funkciju definesana
